ffe57736657387cf32099bfc42e4a2d022e33d6d
Dennis O. Ajalie <ajaliedennis@yahoo.com>
2019-10-19 22:25:52 +0100

    "added knex"

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
+var knex = require('knex');
+const db = knex({
+    client: 'pg',
+    version: '7.2',
+    connection: {
+      host : '127.0.0.1',
+      user : 'your_database_user',
+      password : 'your_database_password',
+      database : 'myapp_test'
+    }
+  });
 
 
 app.get('/', (req, res) => {
