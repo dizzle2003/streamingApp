0df23379c67b4cccdb0b16a0c1938b85344f72a4
Dennis O. Ajalie <ajaliedennis@yahoo.com>
2019-10-22 09:45:22 +0100

    yesterday's work

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ I should be able to see audio files uploaded by other user
 
 const express = require('express');
 const app = express();
+const fs = require('fs');
 const cors = require('cors');
 const bcrypt = require('bcrypt');
 const PORT = process.env.PORT || 5000;
@@ -23,6 +24,7 @@ const db = knex({
       database : 'audiophile'
     }
   });
+const { Readable } = require('stream');
 
 app.use(cors());
 app.use(express.json());
@@ -36,7 +38,9 @@ app.get('/', (req, res) => {
 //Registration Route
 app.post('/register', (req, res) => {
     const {firstname, lastname, username, genre, email, password} = req.body;
-    db('users').insert({
+    db('users')
+    .returning('*')
+    .insert({
         first_name: firstname,
         last_name: lastname,
         username: username,
@@ -44,7 +48,12 @@ app.post('/register', (req, res) => {
         preferred_genre: genre,
         joined: new Date()
     })
-    .then(console.log);
+    .then(user => {
+        res.json(user[0]);
+    })
+    .catch(err => {
+        res.status(400).json("Unable to register!")
+    })
 });
 
 //Sign-in Route
@@ -55,6 +64,7 @@ app.post('/sign-in', (req, res) => {
 //Upload and audio file
 app.post('/upload', (req, res) => {
     const {track_id, title, artist, audio_file, composer, media_type, genre, bytes} = req.body;
+
 });
 
 //Play audio
